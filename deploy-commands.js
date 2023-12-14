65495ee50d14ef5d8b3c7129dd5d45eab09828ea
ARAN1218 <nara1218@icloud.com>
2023-12-15 03:08:01 +0900

    first commit

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,8 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+// const { clientId, guildId, token } = require('./config.json');
+const token = process.env.DISCORD_TOKEN
+const clientId = process.env.DISCORD_CLIENTID
+const guildId = process.env.DISCORD_GUILDID
 const fs = require('node:fs');
 const path = require('node:path');
 
